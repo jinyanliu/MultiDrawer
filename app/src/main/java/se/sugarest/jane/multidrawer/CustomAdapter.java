adc0b822c8c11dd1539e4486f8b8e38f0f17c4eb
jinyanliu <jane.jinyan.liu@gmail.com>
2017-08-24 15:23:57 +0200

    Implement Multi Drawer Navigation.

---
+++ app/src/main/java/se/sugarest/jane/multidrawer/CustomAdapter.java
@@ -0,0 +1,65 @@
+package se.sugarest.jane.multidrawer;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by jane on 17-8-24.
+ */
+
+public class CustomAdapter extends BaseAdapter {
+
+    String mResult[];
+    Context mContext;
+    int img[];
+
+    private static LayoutInflater mInflater = null;
+
+    public CustomAdapter(String[] mResult, Context mContext, int[] img) {
+        this.mResult = mResult;
+        this.mContext = mContext;
+        this.img = img;
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return mResult.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Holder holder = new Holder();
+        View rootView;
+        rootView = mInflater.inflate(R.layout.gdmain, null);
+        holder.tv = (TextView) rootView.findViewById(R.id.tv_hello_world);
+        holder.iv = (ImageView) rootView.findViewById(R.id.iv_five);
+        holder.tv.setText(mResult[position]);
+        holder.iv.setImageResource(img[position]);
+        holder.tv.setTextColor(Color.BLUE);
+        return rootView;
+    }
+
+    public class Holder {
+        TextView tv;
+        ImageView iv;
+    }
+
+
+}
--- app/src/main/java/se/sugarest/jane/multidrawer/MainActivity.java
+++ app/src/main/java/se/sugarest/jane/multidrawer/MainActivity.java
@@ -1,13 +1,90 @@
 package se.sugarest.jane.multidrawer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar mToolBar;
+    DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle mDrawerToggle;
+    ImageButton mImageButtonLeft, mImageButtonRight;
+    GridView mGridViewMain, mGridViewLeft, mGridViewRight;
+    CustomAdapter mCustomAdapter;
+    int img[];
+    String name[];
+    String j;
+    Context mContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
+        mGridViewMain = (GridView) findViewById(R.id.main_grid);
+        mGridViewLeft = (GridView) findViewById(R.id.drawer_left);
+        mGridViewRight = (GridView) findViewById(R.id.drawer_right);
+        mImageButtonLeft = (ImageButton) findViewById(R.id.img_Left);
+        mImageButtonRight = (ImageButton) findViewById(R.id.img_Right);
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mImageButtonLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mDrawerLayout.isDrawerOpen(mGridViewLeft)) {
+                    mDrawerLayout.closeDrawer(mGridViewLeft);
+                } else if (!mDrawerLayout.isDrawerOpen(mGridViewLeft)) {
+                    mDrawerLayout.openDrawer(mGridViewLeft);
+                }
+                if (mDrawerLayout.isDrawerOpen(mGridViewRight)) {
+                    mDrawerLayout.closeDrawer(mGridViewRight);
+                }
+            }
+        });
+
+        mImageButtonRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mDrawerLayout.isDrawerOpen(mGridViewRight)) {
+                    mDrawerLayout.closeDrawer(mGridViewRight);
+                } else if (!mDrawerLayout.isDrawerOpen(mGridViewRight)) {
+                    mDrawerLayout.openDrawer(mGridViewRight);
+                }
+                if (mDrawerLayout.isDrawerOpen(mGridViewLeft)) {
+                    mDrawerLayout.closeDrawer(mGridViewLeft);
+                }
+
+            }
+        });
+
+        getData();
+
+    }
+
+    public void getData() {
+        j = "Cloud, Moon, Umbrella, Flower";
+        name = j.split(",");
+        img = new int[]{
+                R.drawable.ic_one,
+                R.drawable.ic_two,
+                R.drawable.ic_three,
+                R.drawable.ic_four};
+        mCustomAdapter = new CustomAdapter(name, mContext, img);
+        mGridViewMain.setAdapter(mCustomAdapter);
+        mGridViewLeft.setAdapter(mCustomAdapter);
+        mGridViewRight.setAdapter(mCustomAdapter);
     }
 }
